36cbf250be6c661e21a2753d4daf117682bbf27c
Maroooo <rado.dayef@gmail.com>
2026-04-29 12:54:58 +0300

    Scheduled Notification Version

--- lib/noti_controller.dart
+++ lib/noti_controller.dart
@@ -1,4 +1,7 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+import 'package:flutter_timezone/flutter_timezone.dart';
 
 class NotiController {
   static final FlutterLocalNotificationsPlugin _plugin = FlutterLocalNotificationsPlugin();
@@ -9,12 +12,16 @@ class NotiController {
     InitializationSettings settings = InitializationSettings(android: android);
     await _plugin.initialize(settings);
     await requestPermission();
+    tz.initializeTimeZones();
+    final String timeZoneName = await FlutterTimezone.getLocalTimezone();
+    tz.setLocalLocation(tz.getLocation(timeZoneName));
   }
 
   /// Request permission
   static Future<void> requestPermission() async {
     final androidImp = _plugin.resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>();
     if (androidImp != null) {
+      await androidImp.requestExactAlarmsPermission();
       await androidImp.requestNotificationsPermission();
     }
   }
@@ -22,17 +29,38 @@ class NotiController {
   /// Show instance notification
   static Future<void> showInstanceNotification({required int id, required String body, required String title}) async {
     AndroidNotificationDetails androidDetails = AndroidNotificationDetails(
-      "important_chanel",
-      "Important Channel",
+      "instance_chanel",
+      "Instance Channel",
       showWhen: true,
       priority: Priority.high,
       importance: Importance.max,
-      channelDescription: "Important Notifications Channel",
+      channelDescription: "Instance Notifications Channel",
     );
     NotificationDetails details = NotificationDetails(android: androidDetails);
+
     await _plugin.show(id, title, body, details);
   }
 
   /// Show scheduled notification
-  void showScheduledNotification() {}
+  static Future<void> showScheduledNotification({required int id, required String body, required String title, required DateTime schedule}) async {
+    AndroidNotificationDetails androidDetails = AndroidNotificationDetails(
+      "scheduled_chanel",
+      "Scheduled Channel",
+      showWhen: true,
+      priority: Priority.high,
+      importance: Importance.max,
+      channelDescription: "Scheduled Notifications Channel",
+    );
+    NotificationDetails details = NotificationDetails(android: androidDetails);
+
+    await _plugin.zonedSchedule(
+      id,
+      title,
+      body,
+      tz.TZDateTime.from(schedule, tz.local),
+      details,
+      androidScheduleMode: AndroidScheduleMode.inexactAllowWhileIdle,
+      uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime,
+    );
+  }
 }
--- lib/test_local_noti_app.dart
+++ lib/test_local_noti_app.dart
@@ -9,11 +9,28 @@ class TestLocalNotiApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: ElevatedButton(
-            onPressed: () async {
-              await NotiController.showInstanceNotification(id: 0, body: "This is an instance notification", title: "Instance Notification");
-            },
-            child: Text("Send Notification"),
+          child: Column(
+            mainAxisAlignment: .center,
+            children: [
+              ElevatedButton(
+                onPressed: () async {
+                  await NotiController.showInstanceNotification(id: 0, body: "This is an instance notification", title: "Instance Notification");
+                },
+                child: Text("Send Instance Notification"),
+              ),
+              SizedBox(height: 24),
+              ElevatedButton(
+                onPressed: () async {
+                  await NotiController.showScheduledNotification(
+                    id: 0,
+                    body: "This is an Scheduled notification",
+                    title: "Scheduled Notification",
+                    schedule: DateTime.now().add(Duration(seconds: 10)),
+                  );
+                },
+                child: Text("Send Scheduled Notification"),
+              ),
+            ],
           ),
         ),
       ),
